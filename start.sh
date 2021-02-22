dd60671054f836e6c812bbfa7ffabe603110bc6f
Javier Rodriguez <javrodri@student.42madrid.com>
2021-02-22 15:56:31 +0100

    update script

--- start.sh
+++ start.sh
@@ -8,7 +8,14 @@ cyan=$'\e[0;1;36;40m'
 magenta=$'\e[0;1;95;40m'
 nc=$'\e[0m'
 
-echo "${green}${nc}"
+
+echo "${green}Cleaning....${nc}"
+kubectl delete pod --all
+kubectl delete deployment --all
+kubectl delete service --all
+kubectl delete ingress --all
+
+echo "${green}Starting minikube....${nc}"
 mkdir /goinfre/$USER/.minikube
 ln -s /goinfre/$USER/.minikube ~/.minikube
 minikube start --vm-driver=virtualbox --cpus 3 --memory=3000mb
