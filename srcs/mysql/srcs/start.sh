a0b18ceffacc2d2db94950f5b7647e72c1c3bab6
Javier Rodriguez <javrodri@student.42madrid.com>
2021-03-08 17:09:12 +0100

    mysql working

--- srcs/mysql/srcs/start.sh
+++ srcs/mysql/srcs/start.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
+#telegraf &
+# Setup MySQL
+/usr/bin/mysql_install_db --datadir=/var/lib/mysql
 
-#Install MariaDB database(mariadb-install-db is a symlink
-mariadb-install-db -u root
+# Start MySQL in background
+/usr/bin/mysqld --user=root --init_file=/init_file & sleep 3
+
+/usr/bin/mysqld_safe --datadir='/var/lib/mysql'
+
+# Initialize database
+mysql wordpress -u root < wordpress.sql
+
+# Keep container running
+tail -f /dev/null
