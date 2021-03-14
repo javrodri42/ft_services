06c793f4c0be644e0c223733af81abf997ff45bd
Javier Rodriguez Garcia <javier@pop-os.localdomain>
2021-03-14 23:33:01 +0100

    update sh

--- setup.sh
+++ setup.sh
@@ -11,6 +11,16 @@ magenta=$'\e[0;1;95;40m'
 yellow=$'\e[0;92;33m'
 nc=$'\e[0m'
 
+
+printf"   __ _                         _                 "
+printf"  / _| |                       (_)                "
+printf" | |_| |_   ___  ___ _ ____   ___  ___ ___  ___   "
+printf" |  _| __| / __|/ _ \ '__\ \ / / |/ __/ _ \/ __|  "
+printf" | | | |_  \__ \  __/ |   \ V /| | (_|  __/\__ \  "
+printf" |_|  \__| |___/\___|_|    \_/ |_|\___\___||___/  "
+printf"        ______                                    "
+printf"---------------------------------------------------"
+
 testftps (){
 	eval $(minikube docker-env)
   	echo "${yellow}Testing ftps...${nc}"
@@ -35,7 +45,7 @@ clean (){
 	fi
 	if [[ $(kubectl get services) ]]
         then
-                kubectl delete service --all
+        kubectl delete service --all
      fi
      kubectl delete deployment --all
      kubectl delete ingress --all
@@ -79,12 +89,19 @@ eval $(minikube docker-env)
 
 echo "${green}OK${nc}"
 echo "${blue}Creating images....${nc}"
+echo "${yellow}Nginx image...${nc}"
 docker build -t nginx       ./srcs/nginx 		>> logs.txt
+echo "${yellow}PhpMyAdmin image... ${nc}"
 docker build -t phpmyadmin  ./srcs/phpmyadmin 	>> logs.txt
+echo "${yellow}Wordpress image... ${nc}"
 docker build -t wordpress   ./srcs/wordpress 	>> logs.txt
+echo "${yellow}Mysql image... ${nc}"
 docker build -t mysql       ./srcs/mysql 		>> logs.txt
+echo "${yellow}Ftps image... ${nc}"
 docker build -t ftps        ./srcs/ftps 		>> logs.txt
+echo "${yellow}InfluxDB image... ${nc}"
 docker build -t influxdb    ./srcs/influxdb 	>> logs.txt
+echo "${yellow}Grafana image... ${nc}"
 docker build -t grafana    ./srcs/grafana 		>> logs.txt
 echo "${green}OK${nc}"
 
@@ -109,3 +126,12 @@ kubectl get services
 echo "--------------------------------------------------------------------------------------"
 kubectl get pod
 
+echo "--------------------------------------------------------------------------------------"
+
+echo -e $GREEN
+echo "----------------------------------------------------------------------------------------------"
+echo "| Services   | PHPMyAdmin    | InfluxDB      | FTPS          | Wordpress     | Grafana       |"
+echo "|--------------------------------------------------------------------------------------------|"
+echo "| Login      | admin         | graf_admin    | user          | cclaude       | admin         |"
+echo "| Password   | admin         | 10101         | user          | cclaude1      | passwd        |"
+echo "----------------------------------------------------------------------------------------------"
