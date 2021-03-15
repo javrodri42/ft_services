45ffb2fd90a3c08dbd59166d9b13ebad52ca6559
Javier Rodriguez <javrodri@student.42madrid.com>
2021-03-15 19:37:08 +0100

    Update

--- setup.sh
+++ setup.sh
@@ -11,15 +11,17 @@ magenta=$'\e[0;1;95;40m'
 yellow=$'\e[0;92;33m'
 nc=$'\e[0m'
 
-
-printf"   __ _                         _                 "
-printf"  / _| |                       (_)                "
-printf" | |_| |_   ___  ___ _ ____   ___  ___ ___  ___   "
-printf" |  _| __| / __|/ _ \ '__\ \ / / |/ __/ _ \/ __|  "
-printf" | | | |_  \__ \  __/ |   \ V /| | (_|  __/\__ \  "
-printf" |_|  \__| |___/\___|_|    \_/ |_|\___\___||___/  "
-printf"        ______                                    "
-printf"---------------------------------------------------"
+clear
+echo ${green}
+echo "   __ _                         _                 "
+echo "  / _| |                       (_)                "
+echo " | |_| |_   ___  ___ _ ____   ___  ___ ___  ___   "
+echo " |  _| __| / __|/ _ \ '__\ \ / / |/ __/ _ \/ __|  "
+echo " | | | |_  \__ \  __/ |   \ V /| | (_|  __/\__ \  "
+echo " |_|  \__| |___/\___|_|    \_/ |_|\___\___||___/  "
+echo "        ______                                    "
+echo "---------------------------------------------------"
+echo ${nc}
 
 testftps (){
 	eval $(minikube docker-env)
@@ -50,12 +52,13 @@ clean (){
      kubectl delete deployment --all
      kubectl delete ingress --all
      kubectl delete -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml >> logs.txt
-	 minikube delete
-	 killall -TERM kubectl minikube VBoxHeadless
- 	 docker system prune -af
+	 minikube delete  >> logs.txt
+	 killall -TERM kubectl minikube VBoxHeadless  >> logs.txt
+ 	 docker system prune -af					>> logs.txt
 	 echo "${nc}${green}-Cleaned-${nc}"
 
 }
+
 
 if [[ $1 = "ftps" ]]
 then
@@ -67,29 +70,35 @@ if [[ $1 = "restart"  ]]
 then
   	echo "${blue}Restarting...${nc}"
 	eval $(minikube docker-env)
+	echo "${green}---DONE---${nc}"
 	clean
+fi
+
+if [[ $1 = "clean"  ]]
+then
+  	echo "${blue}Cleaning...${nc}"
+	eval $(minikube docker-env)
+	clean
+	echo "${green}---DONE---${nc}"
+	exit
 fi
 
 if [ $(minikube status | grep -c "Running") = 0 ]
 then
 	echo "${green}Starting minikube....${nc}"
-	minikube start --driver=virtualbox
-	minikube addons enable metrics-server
-	minikube addons enable ingress
-	minikube addons enable dashboard
-	minikube addons enable metallb
+	minikube start --driver=virtualbox			>> logs.txt   
+	minikube addons enable metrics-server		>> logs.txt
+	minikube addons enable ingress				>> logs.txt
+	minikube addons enable dashboard			>> logs.txt
+	minikube addons enable metallb				>> logs.txt
+	echo "${green}---DONE---${nc}"
+else
+	echo "${green}Minikube already started${nc}"
 fi
 
 eval $(minikube docker-env)
-
-#echo "${blue}Starting MetalLB...${nc}"
-#kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
-#kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
-#kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-
-echo "${green}OK${nc}"
 echo "${blue}Creating images....${nc}"
-echo "${yellow}Nginx image...${nc}"
+echo "${yellow}Nginx image...${nc}" 
 docker build -t nginx       ./srcs/nginx 		>> logs.txt
 echo "${yellow}PhpMyAdmin image... ${nc}"
 docker build -t phpmyadmin  ./srcs/phpmyadmin 	>> logs.txt
@@ -103,35 +112,40 @@ echo "${yellow}InfluxDB image... ${nc}"
 docker build -t influxdb    ./srcs/influxdb 	>> logs.txt
 echo "${yellow}Grafana image... ${nc}"
 docker build -t grafana    ./srcs/grafana 		>> logs.txt
-echo "${green}OK${nc}"
+echo "${green}---DONE---${nc}"
 
 sleep 2 
+
 echo "${blue}Deploying services....${nc}"
-kubectl apply -f srcs/metallb.yaml
-kubectl apply -f srcs/ftps-config.yaml
-kubectl apply -f srcs/ftps.yaml
-kubectl apply -f srcs/grafana.yaml
-kubectl apply -f srcs/grafana-config.yaml
-kubectl apply -f srcs/nginx.yaml 
-kubectl apply -f srcs/mysql.yaml 
-kubectl apply -f srcs/phpmyadmin.yaml
-kubectl apply -f srcs/wordpress.yaml
-kubectl apply -f srcs/influxdb.yaml
-
-
-echo "${green}OK"
+echo "${yellow}Deploying metallb... ${nc}"
+kubectl apply -f srcs/metallb.yaml				>>logs.txt
+echo "${yellow}Deploying ftps... ${nc}"
+kubectl apply -f srcs/ftps-config.yaml			>>logs.txt
+kubectl apply -f srcs/ftps.yaml					>>logs.txt
+echo "${yellow}Deploying grafana... ${nc}"
+kubectl apply -f srcs/grafana.yaml				>>logs.txt
+kubectl apply -f srcs/grafana-config.yaml		>>logs.txt
+echo "${yellow}Deploying nginx... ${nc}"
+kubectl apply -f srcs/nginx.yaml 				>>logs.txt
+echo "${yellow}Deploying mysql... ${nc}"
+kubectl apply -f srcs/mysql.yaml 				>>logs.txt
+echo "${yellow}Deploying phpmyadmin... ${nc}"
+kubectl apply -f srcs/phpmyadmin.yaml			>>logs.txt
+echo "${yellow}Deploying wordpress... ${nc}"
+kubectl apply -f srcs/wordpress.yaml			>>logs.txt
+echo "${yellow}Deploying influxdb... ${nc}"
+kubectl apply -f srcs/influxdb.yaml				>>logs.txt
+echo "${green}---DONE---"
 sleep 7
-
 kubectl get services
 echo "--------------------------------------------------------------------------------------"
 kubectl get pod
-
 echo "--------------------------------------------------------------------------------------"
 
-echo -e $GREEN
-echo "----------------------------------------------------------------------------------------------"
-echo "| Services   | PHPMyAdmin    | InfluxDB      | FTPS          | Wordpress     | Grafana       |"
-echo "|--------------------------------------------------------------------------------------------|"
-echo "| Login      | admin         | graf_admin    | user          | cclaude       | admin         |"
-echo "| Password   | admin         | 10101         | user          | cclaude1      | passwd        |"
-echo "----------------------------------------------------------------------------------------------"
+echo $GREEN
+echo "------------------------------------------------------------------------------"
+echo "| Services   | PHPMyAdmin    | FTPS          | Wordpress     | Grafana       |"
+echo "|----------------------------------------------------------------------------|"
+echo "| Login      | admin         | user          | javrodri      | admin         |"
+echo "| Password   | admin         | user          | passwd        | passwd        |"
+echo "------------------------------------------------------------------------------"
